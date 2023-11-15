4a14fe4c0fe95a06173b5fa893bc0a80d8f3a6a1
Dmitriy <38104472+Nonal007@users.noreply.github.com>
2023-11-15 10:06:18 +0300

    1.0.2
    Update project. Integrate LocalDB

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -17,24 +17,30 @@ using System.Configuration;
 
 namespace Internet_Shop
 {
+
     /// <summary>
     /// Логика взаимодействия для Login.xaml
     /// </summary>
     public partial class Login : Window
     {
+        string conntectionString;
+
         public Login()
         {
             InitializeComponent();
+            conntectionString = ConfigurationManager.ConnectionStrings["Internet_Shop"].ConnectionString;
         }
-
-        DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
-                                                                        // подключаемся к базе данных
-        SqlConnection sqlConnection = new SqlConnection("server=DESKTOP-NBH5JF2;Trusted_Connection=Yes;DataBase=TEST;");
-        sqlConnection.Open();                                           // открываем базу данных
-    SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
-        sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
-    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
-        sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
-    return dataTable;
+        public DataTable ConnectDB(string selectSQL)
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection(conntectionString);
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            return dataTable;
+        }
     }
 }
